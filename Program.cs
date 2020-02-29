d2ec756e61050ac27fcd68470460f18d65cbadd4
Rafael16042004 <rafafelipefigueiredo@gmail.com>
2020-02-28 22:45:52 -0300

    letra da musica

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace musicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Evidencias");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ");
+            Console.WriteLine("Quando eu digo que deixei de te amar");
+            Console.WriteLine("É porque eu te amo");
+            Console.WriteLine("Quando eu digo que não quero mais você");
+            Console.WriteLine("É porque eu te quero");
+            Console.WriteLine("Eu tenho medo de te dar meu coração");
+            Console.WriteLine("E confessar que eu estou em tuas mãos");
+            Console.WriteLine("Mas não posso imaginar o que vai ser de mim");
+            Console.WriteLine("Se eu te perder um dia ");
+            Console.WriteLine(" ");
+            Console.WriteLine("Eu me afasto e me defendo de você");
+            Console.WriteLine("Mas depois me entrego");
+            Console.WriteLine("Faço tipo, falo coisas que eu não sou");
+            Console.WriteLine("Mas depois eu nego");
+            Console.WriteLine("Mas a verdade é que eu sou louco por vocês");
+            Console.WriteLine("E tenho medo de pensar em te perder");
+            Console.WriteLine("Eu preciso aceitar que não dá mais");
+            Console.WriteLine("Pra separar as nossas vidas");
+            Console.WriteLine(" ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("E nessa loucura de dizer que não te quero");
+            Console.WriteLine("Vou negando as aparências");
+            Console.WriteLine("Disfarçando as evidências");
+            Console.WriteLine("Mas pra que viver fingindo");
+            Console.WriteLine("Se eu não posso enganar meu coração");
+            Console.WriteLine("Eu sei que te amo");
+            Console.WriteLine("Chega de mentiras");
+            Console.WriteLine("De negar o meu desejo");
+            Console.WriteLine("Eu te quero mais que tudo");
+            Console.WriteLine("Eu preciso do seu beijo");
+            Console.WriteLine("Eu entrego a minha vida");
+            Console.WriteLine("Pra você fazer o que quiser de mim");
+            Console.WriteLine("Só quero ouvir você dizer que sim");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+
+
+        }
+    }
+}
